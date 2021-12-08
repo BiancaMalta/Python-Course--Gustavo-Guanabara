7750d9f6f722a1cf49223794a09aae6ebbb17f6f
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2021-12-08 10:25:48 -0300

    Create Python_Exercise_09.py

---
+++ World 1 - Fundamentals/Python_Exercise_09.py
@@ -0,0 +1,16 @@
+#Python Exercise 09
+
+#Develop a program that reads an integer and displays its multiplication table on the screen.
+n = int(input(f'Enter the number to see its multiplication table: '))
+
+print(f'{n} x 01 = {n * 1} \n'
+      f'{n} x 02 = {n * 2} \n'
+      f'{n} x 03 = {n * 3} \n'
+      f'{n} x 04 = {n * 4} \n'
+      f'{n} x 05 = {n * 5} \n'
+      f'{n} x 06 = {n * 6} \n'
+      f'{n} x 07 = {n * 7} \n'
+      f'{n} x 08 = {n * 8} \n'
+      f'{n} x 09 = {n * 9} \n'
+      f'{n} x 10 = {n * 10}')
+
