68a3a05293c3f9aaa59d35b421a54a9bbb46a345
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2021-12-07 21:49:25 -0300

    Create Python_Exercise_03.py

---
+++ World 1 - Fundamentals/Python_Exercise_03.py
@@ -0,0 +1,7 @@
+#Python Exercise 03
+
+#Create a program that reads two numbers and shows the sum between them.
+num1 = int(input('Enter a value: '))
+num2 = int(input('Enter another value: '))
+sum = num1 + num2
+print(f'Sum between {num1} and {num2} is equal to: {sum}!')
