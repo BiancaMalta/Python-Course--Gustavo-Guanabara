7906073247d8a3c3ab8ac6bcb7fc56ac95ba8da7
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2021-12-08 17:46:05 -0300

    Create Python_Exercise_30.py

---
+++ World 1 - Fundamentals/Python_Exercise_30.py
@@ -0,0 +1,9 @@
+#Python Exercise 30
+
+#Create a program that reads an integer and shows on the screen whether it is EVEN or ODD.
+n = int(input('Enter a number: '))
+
+if n % 2 == 0:
+    print('EVEN')
+else:
+    print('ODD')
