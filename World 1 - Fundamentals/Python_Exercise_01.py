6da78ccbd5db2827b4016f19b5e7f2f1688400ce
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2021-12-07 21:41:53 -0300

    Update Python_Exercise_01.py

--- World 1 - Fundamentals/Python_Exercise_01.py
+++ World 1 - Fundamentals/Python_Exercise_01.py
@@ -1,7 +1,4 @@
 #Python Exercise 01
-#Create a Python script that reads a person's name and displays a welcome message.
-name = input('Enter name: \n')
-print(f'Hello, {name}! Nice to meet you!')
 
 #Print "Hello, World" on the screen.
 msg = 'Hello, world!'
