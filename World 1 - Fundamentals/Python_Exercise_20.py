b53789d091587cfdc2436217458cf3d0664b744c
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2021-12-08 15:35:45 -0300

    Create Python_Exercise_20.py

---
+++ World 1 - Fundamentals/Python_Exercise_20.py
@@ -0,0 +1,11 @@
+#Python Exercise 20
+
+#Make a program that reads the names of the four students and shows the order drawn.
+from random import shuffle
+student1 = input('Student 1: ')
+student2 = input('Student 2: ')
+student3 = input('Student 3: ')
+student4 = input('Student 4: ')
+slist = [student1, student2, student3, student4]
+shuffle(slist)
+print(f'The order drawn is: {slist}')
