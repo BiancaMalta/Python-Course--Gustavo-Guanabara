3f4334b275c1828ca003bc16eafafd1eb53cf917
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2021-12-08 09:32:57 -0300

    Create Python_Exercise_07.py

---
+++ World 1 - Fundamentals/Python_Exercise_07.py
@@ -0,0 +1,6 @@
+#Python Exercise 07
+
+#Develop a program that reads a student's two grades, calculates and displays their arithmetic mean.
+g1 = float(input('1st grade: '))
+g2 = float(input('2nd grade: '))
+print(f'The arithmetic mean between {g1} and {g2} is {((g1 + g2) / 2) :.1f}')
