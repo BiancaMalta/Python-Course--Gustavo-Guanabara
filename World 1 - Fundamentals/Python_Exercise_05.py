7fe7c2185dfd78edcd178e8220c6cc6685035646
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2021-12-07 22:21:13 -0300

    Create  Python_Exercise_05.py

---
+++ World 1 - Fundamentals/Python_Exercise_05.py
@@ -0,0 +1,6 @@
+#Python Exercise 05
+
+#Make a program that reads an integer and displays its successor and predecessor on the screen.
+
+i = int(input('Enter a integer: '))
+print(f'The successor of {i} is {i+1}, and the predecessor is {i-1}!')
