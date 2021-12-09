f21378e9bb42a0e9286c452d4a8ea692c90b6864
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2021-12-09 10:58:35 -0300

    Update Python_Exercise_33.py

--- World 1 - Fundamentals/Python_Exercise_33.py
+++ World 1 - Fundamentals/Python_Exercise_33.py
@@ -1,6 +1,6 @@
 #Python Exercise 33
 
-#Make a program that reads three values and shows which is biggest and which is smaller.
+#Make a program that reads three values and shows which is the biggest and which is the smallest.
 print('Enter three integers:')
 n1 = input('number1 > ').strip()
 n2 = input('number2 > ').strip()
@@ -14,8 +14,8 @@ elif n3 > n1 and n3 > n2:
     print(f'The biggest is {n3}')
 
 if n1 < n2 and n1 < n3:
-    print(f'The smaller is {n1}')
+    print(f'The smallest is {n1}')
 elif n2 < n1 and n2 < n3:
-    print(f'The smaller is {n2}')
+    print(f'The smallest is {n2}')
 elif n3 < n1 and n3 < n2:
-    print(f'The smaller is {n3}')
+    print(f'The smallest is {n3}')
