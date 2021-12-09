b5bcb5b565ce66ddd790ddd81210b44249f87d7e
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2021-12-09 10:31:48 -0300

    Update Python_Exercise_14.py

--- World 1 - Fundamentals/Python_Exercise_14.py
+++ World 1 - Fundamentals/Python_Exercise_14.py
@@ -1,5 +1,5 @@
 #Python Exercise 14
 
-#Write a Python program to convert temperatures from Celsius to Fahrenheit.
+#Write a Python program to convert temperatures from Celsius into Fahrenheit.
 c = float(input('Enter a temperature in °C: '))
 print(f'{c}°C corresponds to {c * 1.8 + 32}°F')
