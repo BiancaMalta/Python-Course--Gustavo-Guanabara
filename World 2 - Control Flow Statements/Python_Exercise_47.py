6b0824fe832c175195cd4c6ece64cf4dc06db453
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2021-12-10 09:23:29 -0300

    Update Python_Exercise_47.py

--- World 2 - Control Flow Statements/Python_Exercise_47.py
+++ World 2 - Control Flow Statements/Python_Exercise_47.py
@@ -3,7 +3,7 @@
 #Create a program that displays all the even numbers in the range from 1 to 50 on the screen.
 from time import sleep
 
-r= str(input('Do you like see all even numbers from 1 to 50?[Y/N]')).strip().upper()[0]
+r= str(input('Would you like see all even numbers from 1 to 50?[Y/N]')).strip().upper()[0]
 if r == "Y":
   for c in range(2, 51, 2):
     print(c, end=' ')
